4bbd8c0e28d93cc52c46caeb673efe3288f7e280
Nicolas Lee <nicolasmatthewlee@gmail.com>
2023-01-24 14:15:33 -0800

    delete app.js

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
-import "bootstrap";
-
-const App = () => {
-  return <div className="App"></div>;
-};
-
-export default App;

2854cbb111af166a698283ff3ff07cd23ab57b0e
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2023-01-26 13:47:45 -0800

    properly implemented 404 handler to create an error and pass it on to the error handler with next, which checks if the error.status is 404 or not to send the appropriate error message back. Also implemented data retrieval from mongoDB on /api; was experiencing mongoDB connection error that was resolved by adding my current IP address to network access in mongoDB atlas, as I had moved locations from where I initially began this project

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+
+const Item = require("./models/item");
+
+// connect to mongoDB
+const connectionString = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.yj7r55q.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(connectionString);
+mongoose.connection.on(
+  "error",
+  console.error.bind(console, "MongoDB connection error:")
+);
 
 // create express application
 const app = express();
@@ -15,15 +28,33 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-// handle 404 response
-app.use((req, res) => {
-  res.status(404).send("No resource found.");
+// express automatically catches errors occurring in synchronous code
+// (will be handled by error handler defined after all routes)
+
+// errors returned by asynchronous functions must be passed into the
+// next() function
+// ex) if (err) next(err)
+app.get("/api", (req, res, next) => {
+  Item.find().exec((err, items) => {
+    if (err) return next(err);
+    // no error
+    res.send(items);
+  });
+});
+
+// 404 responses are not captured by express middleware
+// create 404 error and forward to error handler with next()
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
 // error handler
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("An error occured.");
+  err.status === 404
+    ? res.status(404).send("Resource not found.")
+    : res.status(500).send("An unknown error occured.");
 });
 
 // listen for requests at the specified port (with callback on start listening)

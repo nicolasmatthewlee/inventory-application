2673ddeddf8328aca91a1297f1f5a080518f552c
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2023-01-26 17:36:35 -0800

    added htmlFor to each label to match input id; connected form submit to onSubmit() with form attribute onSubmit={onSubmit}; setting the onClick attribute of the submit button resulted in disabling html client-side form validation due to e.preventDefault(), so it was necessary to remove that and instead set the form onSubmit attribute

--- client/src/components/add-item-modal.js
+++ client/src/components/add-item-modal.js
@@ -10,6 +10,7 @@ export const AddItemModal = (props) => {
       const categoriesData = await fetch(`${props.server}/api/categories`);
       const categoriesJSON = await categoriesData.json();
       setCategories(categoriesJSON);
+      setCategory(categoriesJSON[0]);
     } catch (err) {
       console.log(err);
     }
@@ -17,6 +18,17 @@ export const AddItemModal = (props) => {
   useEffect(() => {
     getCategories();
   }, []);
+
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [price, setPrice] = useState(0);
+  const [quantity, setQuantity] = useState(1);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, description, category, price, quantity);
+  };
 
   return (
     <div
@@ -38,22 +50,37 @@ export const AddItemModal = (props) => {
             </div>
           </div>
 
-          <form action="">
+          <form onSubmit={onSubmit}>
             <div className="mb-2">
-              <label className="form-label mb-1">Name</label>
-              <input type="text" className="form-control" placeholder="Name" />
+              {/* htmlFor corresponds the id of the input */}
+              <label htmlFor="name" className="form-label mb-1">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                className="form-control"
+                placeholder="Name"
+                required
+              />
             </div>
             <div className="mb-2">
-              <label className="form-label mb-1">Description</label>
+              <label htmlFor="description" className="form-label mb-1">
+                Description
+              </label>
               <textarea
                 type="text"
+                id="description"
+                rows="3"
                 className="form-control"
                 placeholder="Item description..."
               />
             </div>
             <div className="mb-2">
-              <label className="form-label mb-1">Category</label>
-              <select name="" id="" className="form-select">
+              <label htmlFor="category" className="form-label mb-1">
+                Category
+              </label>
+              <select id="category" className="form-select">
                 {categories.map((c) => (
                   <option key={c}>{c}</option>
                 ))}
@@ -62,11 +89,14 @@ export const AddItemModal = (props) => {
             <div className="row mb-3">
               <div className="col-6">
                 <div>
-                  <label className="form-label mb-1">Price</label>
+                  <label htmlFor="price" className="form-label mb-1">
+                    Price
+                  </label>
                   <div className="input-group">
                     <div className="input-group-text">$</div>
                     <input
                       type="text"
+                      id="price"
                       className="form-control"
                       placeholder="0.00"
                     />
@@ -75,13 +105,22 @@ export const AddItemModal = (props) => {
               </div>
               <div className="col-6">
                 <div>
-                  <label className="form-label mb-1">Quantity</label>
-                  <input type="text" className="form-control" placeholder="1" />
+                  <label htmlFor="quantity" className="form-label mb-1">
+                    Quantity
+                  </label>
+                  <input
+                    type="text"
+                    id="quantity"
+                    className="form-control"
+                    placeholder="1"
+                  />
                 </div>
               </div>
             </div>
 
-            <button className="btn btn-dark w-100">Submit</button>
+            <button type="submit" className="btn btn-dark w-100">
+              Submit
+            </button>
           </form>
         </div>
       </div>

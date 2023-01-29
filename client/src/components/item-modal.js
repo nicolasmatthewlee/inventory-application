7acd7696c8741773772efce57393875ff7f295bd
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2023-01-28 18:16:23 -0800

    fixed ItemModal blocking overlay onClick closing by setting ItemModal positioning with position-fixed, and css style left '25%' which allowed the removal of the container div which was providing horizontal centering with d-flex (but also blocking the overlay below

--- client/src/components/item-modal.js
+++ client/src/components/item-modal.js
@@ -116,199 +116,193 @@ export const ItemModal = (props) => {
 
   return (
     <div
-      className="position-fixed top-0 w-100 zindex-modal d-flex justify-content-center"
-      style={{ marginTop: "100px" }}
+      className="position-fixed card col-10"
+      style={{ maxWidth: "400px", marginTop: "100px", left: "25%" }}
     >
-      <div className="card col-10" style={{ maxWidth: "400px" }}>
-        <div className="card-body p-3">
-          <div className="row">
-            <div className="col">
-              <h2>
-                {props.mode === "add" ? (
-                  <i className="bi-database-fill-add"></i>
-                ) : (
-                  <i className="bi-database-fill-check"> </i>
-                )}
-                {" " + props.mode.charAt(0).toUpperCase() + props.mode.slice(1)}{" "}
-                Item
-              </h2>
-            </div>
-            <div className="col-auto p-0">
-              <button className="btn p-0 me-2" onClick={props.onClose}>
-                <img src={CLOSE_ICON} alt="close" style={{ width: "30px" }} />
-              </button>
-            </div>
+      <div className="card-body p-3">
+        <div className="row">
+          <div className="col">
+            <h2>
+              {props.mode === "add" ? (
+                <i className="bi-database-fill-add"></i>
+              ) : (
+                <i className="bi-database-fill-check"> </i>
+              )}
+              {" " + props.mode.charAt(0).toUpperCase() + props.mode.slice(1)}{" "}
+              Item
+            </h2>
+          </div>
+          <div className="col-auto p-0">
+            <button className="btn p-0 me-2" onClick={props.onClose}>
+              <img src={CLOSE_ICON} alt="close" style={{ width: "30px" }} />
+            </button>
+          </div>
+        </div>
+
+        <form onSubmit={onSubmit}>
+          <div className="mb-2">
+            {/* htmlFor corresponds the id of the input */}
+            <label htmlFor="name" className="form-label mb-1">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="form-control"
+              placeholder="Name"
+              required
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+            />
+          </div>
+          <div className="mb-2">
+            <label htmlFor="description" className="form-label mb-1">
+              Description
+            </label>
+            <textarea
+              type="text"
+              id="description"
+              rows="3"
+              className="form-control"
+              placeholder="Item description..."
+              onChange={(e) => setDescription(e.target.value)}
+              value={description}
+            />
+          </div>
+          <div className="mb-2">
+            <label htmlFor="category" className="form-label mb-1">
+              Category
+            </label>
+            <select
+              id="category"
+              className="form-select"
+              required
+              onChange={(e) => {
+                setCategory(e.target.value);
+                evaluateCategory(e.target.value);
+              }}
+              value={
+                showNewCategoryInput
+                  ? "add new category"
+                  : category
+                  ? category
+                  : ""
+              }
+            >
+              {categories.map((c) => (
+                <option key={c}>{c}</option>
+              ))}
+              {category === null ? null : <option>add new category</option>}
+            </select>
           </div>
 
-          <form onSubmit={onSubmit}>
+          {showNewCategoryInput ? (
             <div className="mb-2">
               {/* htmlFor corresponds the id of the input */}
-              <label htmlFor="name" className="form-label mb-1">
-                Name
+              <label htmlFor="new-category" className="form-label mb-1">
+                New Category
               </label>
               <input
                 type="text"
-                id="name"
+                id="new-category"
                 className="form-control"
-                placeholder="Name"
+                placeholder="Category"
                 required
-                onChange={(e) => setName(e.target.value)}
-                value={name}
+                onChange={(e) => setCategory(e.target.value)}
               />
             </div>
-            <div className="mb-2">
-              <label htmlFor="description" className="form-label mb-1">
-                Description
-              </label>
-              <textarea
-                type="text"
-                id="description"
-                rows="3"
-                className="form-control"
-                placeholder="Item description..."
-                onChange={(e) => setDescription(e.target.value)}
-                value={description}
-              />
-            </div>
-            <div className="mb-2">
-              <label htmlFor="category" className="form-label mb-1">
-                Category
-              </label>
-              <select
-                id="category"
-                className="form-select"
-                required
-                onChange={(e) => {
-                  setCategory(e.target.value);
-                  evaluateCategory(e.target.value);
-                }}
-                value={
-                  showNewCategoryInput
-                    ? "add new category"
-                    : category
-                    ? category
-                    : ""
-                }
-              >
-                {categories.map((c) => (
-                  <option key={c}>{c}</option>
-                ))}
-                {category === null ? null : <option>add new category</option>}
-              </select>
-            </div>
+          ) : null}
 
-            {showNewCategoryInput ? (
-              <div className="mb-2">
-                {/* htmlFor corresponds the id of the input */}
-                <label htmlFor="new-category" className="form-label mb-1">
-                  New Category
+          <div className="row mb-3">
+            <div className="col-6">
+              <div>
+                <label htmlFor="price" className="form-label mb-1">
+                  Price
                 </label>
-                <input
-                  type="text"
-                  id="new-category"
-                  className="form-control"
-                  placeholder="Category"
-                  required
-                  onChange={(e) => setCategory(e.target.value)}
-                />
-              </div>
-            ) : null}
-
-            <div className="row mb-3">
-              <div className="col-6">
-                <div>
-                  <label htmlFor="price" className="form-label mb-1">
-                    Price
-                  </label>
-                  <div className="input-group">
-                    <div className="input-group-text">$</div>
-                    <input
-                      type="number"
-                      id="price"
-                      className="form-control"
-                      placeholder="0.00"
-                      onChange={(e) => setPrice(e.target.value)}
-                      min="0"
-                      style={{
-                        MozAppearance: "textfield",
-                      }}
-                      value={price}
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="col-6">
-                <div>
-                  <label htmlFor="quantity" className="form-label mb-1">
-                    Quantity
-                  </label>
+                <div className="input-group">
+                  <div className="input-group-text">$</div>
                   <input
                     type="number"
-                    id="quantity"
+                    id="price"
                     className="form-control"
-                    placeholder="1"
-                    onChange={(e) => setQuantity(e.target.value)}
+                    placeholder="0.00"
+                    onChange={(e) => setPrice(e.target.value)}
                     min="0"
-                    value={quantity}
+                    style={{
+                      MozAppearance: "textfield",
+                    }}
+                    value={price}
                   />
                 </div>
               </div>
             </div>
-
-            {errors.length > 0 ? (
-              <div className="container-fluid list-group p-0  mb-3">
-                <li className="list-group-item list-group-item-danger m-0">
-                  <ul className="m-0 ps-3">
-                    {errors.map((e) => (
-                      <li className="" key={uniqid()}>
-                        {e.msg}
-                      </li>
-                    ))}
-                  </ul>
-                </li>
+            <div className="col-6">
+              <div>
+                <label htmlFor="quantity" className="form-label mb-1">
+                  Quantity
+                </label>
+                <input
+                  type="number"
+                  id="quantity"
+                  className="form-control"
+                  placeholder="1"
+                  onChange={(e) => setQuantity(e.target.value)}
+                  min="0"
+                  value={quantity}
+                />
               </div>
-            ) : null}
+            </div>
+          </div>
 
-            {props.mode === "add" ? (
-              <button type="submit" className="btn btn-success w-100">
-                {loading ? (
-                  <div>
-                    <i className="spinner-border spinner-border-sm"></i>{" "}
-                    Loading...
-                  </div>
-                ) : (
-                  "Add"
-                )}
-              </button>
-            ) : (
-              <button
-                type="submit"
-                className="btn btn-warning w-100 text-white"
-              >
-                {loading ? (
-                  <div>
-                    <i className="spinner-border spinner-border-sm"></i>{" "}
-                    Loading...
-                  </div>
-                ) : (
-                  "Update"
-                )}
-              </button>
-            )}
-          </form>
-          {props.mode === "update" ? (
-            <button onClick={onDelete} className="btn btn-danger w-100 mt-3">
-              {deleteLoading ? (
+          {errors.length > 0 ? (
+            <div className="container-fluid list-group p-0  mb-3">
+              <li className="list-group-item list-group-item-danger m-0">
+                <ul className="m-0 ps-3">
+                  {errors.map((e) => (
+                    <li className="" key={uniqid()}>
+                      {e.msg}
+                    </li>
+                  ))}
+                </ul>
+              </li>
+            </div>
+          ) : null}
+
+          {props.mode === "add" ? (
+            <button type="submit" className="btn btn-success w-100">
+              {loading ? (
                 <div>
                   <i className="spinner-border spinner-border-sm"></i>{" "}
                   Loading...
                 </div>
               ) : (
-                "Delete"
+                "Add"
               )}
             </button>
-          ) : null}
-        </div>
+          ) : (
+            <button type="submit" className="btn btn-success w-100">
+              {loading ? (
+                <div>
+                  <i className="spinner-border spinner-border-sm"></i>{" "}
+                  Loading...
+                </div>
+              ) : (
+                "Update"
+              )}
+            </button>
+          )}
+        </form>
+        {props.mode === "update" ? (
+          <button onClick={onDelete} className="btn btn-danger w-100 mt-3">
+            {deleteLoading ? (
+              <div>
+                <i className="spinner-border spinner-border-sm"></i> Loading...
+              </div>
+            ) : (
+              "Delete"
+            )}
+          </button>
+        ) : null}
       </div>
     </div>
   );
